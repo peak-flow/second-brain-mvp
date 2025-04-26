b4480dab30a8e84e1b51d0c5bee5ab20ccca81ec
david abraham <dave@springbasemedia.com>
2025-04-25 22:09:07 -0400

    test(config): configure Pest and base TestCase for database and CSRF handling

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -3,8 +3,17 @@
 namespace Tests;
 
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
+use Illuminate\Foundation\Http\Middleware\VerifyCsrfToken;
 
 abstract class TestCase extends BaseTestCase
 {
-    //
+    /**
+     * Set up the test environment.
+     */
+    protected function setUp(): void
+    {
+        parent::setUp();
+        // Disable CSRF middleware for testing
+        $this->withoutMiddleware(VerifyCsrfToken::class);
+    }
 }
